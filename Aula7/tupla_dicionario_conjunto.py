a06948dcd5923938309a563cc4b2c4902d3529fb
TalitaCipriano <talitacfmc@gmail.com>
2025-08-26 19:17:29 +0000

    tupla

---
+++ Aula7/tupla_dicionario_conjunto.py
@@ -0,0 +1,15 @@
+### TUPLA - CONJUNTO - DICIONÁRIO
+#tupla - necessario a ',' para ser uma tupla, caso não tenha, ela é uma str
+
+tupla_vazia = ()
+print(type(tupla_vazia))
+print(tupla_vazia)
+
+tupla = ("casa", 2, [1,2,3], tupla_vazia)
+tupla[2].append(6)
+print(tupla)
+
+tupla_nomes = ("joão", "Pedro", "Sara")
+print(type(tupla_nomes))
+lista_nomes = list(tupla_nomes) #transformanso a tupla em lista
+lista_nomes

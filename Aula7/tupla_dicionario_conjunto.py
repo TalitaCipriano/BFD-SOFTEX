bee3e76e89a418ea7aa5cd98f14501d9171d3124
TalitaCipriano <talitacfmc@gmail.com>
2025-08-28 19:09:23 +0000

    atividade_extra_dict

--- Aula7/tupla_dicionario_conjunto.py
+++ Aula7/tupla_dicionario_conjunto.py
@@ -14,28 +14,3 @@
 # lista_nomes = list(tupla_nomes) #transformanso a tupla em lista
 # lista_nomes
 
-#####    MATCH
-
-#criando um menu com match (CRUD)
-
-while True:
-    opcao = input("Escola uma opção: \n "
-                 "1 - Adicionar um livro \n"
-                 "2 - Editar informaçoes \n"
-                 "3 - Pesquisar um livro \n"
-                 "4 - Deletar um livro \n"
-                 "0 - Sair \n")
- 
-    match opcao:
-        case "1":
-            print("adicionando livro")
-        case "2":
-            print("editando livro")
-        case "3":
-            print("pesquisando livro")
-        case "4":
-            print("deletando livro")
-        case "0":
-            break
-        case _:
-            print("opcao invalida")

8c76d684fb79cfff5cc7c7a16b54296e832c3809
TalitaCipriano <talitacfmc@gmail.com>
2025-08-28 18:43:25 +0000

    atividades

--- Aula7/atividades.py
+++ Aula7/atividades.py
@@ -2,79 +2,106 @@
 
 #1111111111111111111111111111111111111111111111
 
-livros = ["Iracema", "Senhora", "O guarani"]
-print(livros)
+#livros = ["Iracema", "Senhora", "O guarani"]
+#print(livros)
 
 #2222222222222222222222222222222222222222222222
 
-livros.pop(1)
-print(livros)
+# livros.pop(1)
+# print(livros)
 
 #3333333333333333333333333333333333333333333333
 
-livros2 = ("A viuvinha", "Cinco minutos")
-livros.extend(livros2) 
-print(livros)
+# livros2 = ("A viuvinha", "Cinco minutos")
+# livros.extend(livros2) 
+# print(livros)
 
 #4444444444444444444444444444444444444444444444
 
-livros.insert(1,"Duna")
-print(livros)
+# livros.insert(1,"Duna")
+# print(livros)
 
 #5555555555555555555555555555555555555555555555
 
-if "O silencio dos inocentes" in livros:
-    livros.remove("O silencio dos inocentes")
-else:
-    print("Livro não encontrado")
+# if "O silencio dos inocentes" in livros:
+#     livros.remove("O silencio dos inocentes")
+# else:
+#     print("Livro não encontrado")
 
 #6666666666666666666666666666666666666666666666
 
-numeros = [1,2,3,2,4,2,5]
-qtd = numeros.count(2)
-print(qtd)
+# numeros = [1,2,3,2,4,2,5]
+# qtd = numeros.count(2)
+# print(qtd)
 
 #7777777777777777777777777777777777777777777777
 
-for lista in livros:
-    print(f"O livro {lista} é interessante")
+#for lista in livros:
+    #print(f"O livro {lista} é interessante")
 
 #8888888888888888888888888888888888888888888888
 
-idades = [12, 18, 25, 14, 30]
-for lista in idades:
-    if lista >= 18:
-        print(lista)
+# idades = [12, 18, 25, 14, 30]
+# for lista in idades:
+#     if lista >= 18:
+#         print(lista)
 
 #9999999999999999999999999999999999999999999999
 
-valores = [10, 20, 30, 40]
-total = sum(valores)
-print(total)
+# valores = [10, 20, 30, 40]
+# total = sum(valores)
+# print(total)
 
 #1010101010101010101010101010101010101010101010
 
-nota1_aluno1 = float(input("Digite a nota 1 do aluno 1: "))
-nota2_aluno1 = float(input("Digite a nota 2 do aluno 1: "))
-nota3_aluno1 = float(input("Digite a nota 3 do aluno 1: "))
+# nota1_aluno1 = float(input("Digite a nota 1 do aluno 1: "))
+# nota2_aluno1 = float(input("Digite a nota 2 do aluno 1: "))
+# nota3_aluno1 = float(input("Digite a nota 3 do aluno 1: "))
 
-nota1_aluno2 = float(input("Digite a nota 1 do aluno 2: "))
-nota2_aluno2 = float(input("Digite a nota 2 do aluno 2: "))
-nota3_aluno2 = float(input("Digite a nota 3 do aluno 2: "))
+# nota1_aluno2 = float(input("Digite a nota 1 do aluno 2: "))
+# nota2_aluno2 = float(input("Digite a nota 2 do aluno 2: "))
+# nota3_aluno2 = float(input("Digite a nota 3 do aluno 2: "))
 
-notas_aluno1 = [nota1_aluno1, nota2_aluno1, nota3_aluno1]
-print(notas_aluno1)
-notas_aluno2 = [nota1_aluno2, nota2_aluno2, nota3_aluno2]
-print(notas_aluno2)
+# notas_aluno1 = [nota1_aluno1, nota2_aluno1, nota3_aluno1]
+# print(notas_aluno1)
+# notas_aluno2 = [nota1_aluno2, nota2_aluno2, nota3_aluno2]
+# print(notas_aluno2)
 
-notas = [notas_aluno1, notas_aluno2]
-print(notas)
+# notas = [notas_aluno1, notas_aluno2]
+# print(notas)
 
-media_aluno1 = (nota1_aluno1 + nota2_aluno1 + nota3_aluno1) / 3
-print(media_aluno1)
+# media_aluno1 = (nota1_aluno1 + nota2_aluno1 + nota3_aluno1) / 3
+# print(media_aluno1)
 
-media_aluno2 = (nota1_aluno2 + nota2_aluno2 + nota3_aluno2) / 3
-print(media_aluno2)
+# media_aluno2 = (nota1_aluno2 + nota2_aluno2 + nota3_aluno2) / 3
+# print(media_aluno2)
 
 #11#11#11#11#11#11#11#11#11#11#11#11#11#11#11#11#11#11#11
 
+# tabuleiro_vazio = [[None for coluna in range(8)] for linha in range(8)]
+# for linha in tabuleiro_vazio:
+#       print(linha)
+
+# tabuleiro_vazio[0][0] = "tor"
+# tabuleiro_vazio[0][-1] = "tor"
+# tabuleiro_vazio[0][1] = "cav"
+# tabuleiro_vazio[0][-2] = "cav"
+# tabuleiro_vazio[0][2] = "bis"
+# tabuleiro_vazio[0][-3] = "bis"
+# tabuleiro_vazio[0][3] = "rei"
+# tabuleiro_vazio[0][4] = "rai"
+# tabuleiro_vazio[1] = ["pea"] * 8
+
+# tabuleiro_vazio[-1][0] = "TOR"
+# tabuleiro_vazio[-1][-1] = "TOR"
+# tabuleiro_vazio[-1][1] = "CAV"
+# tabuleiro_vazio[-1][-2] = "CAV"
+# tabuleiro_vazio[-1][2] = "BIS"
+# tabuleiro_vazio[-1][-3] = "BIS"
+# tabuleiro_vazio[-1][3] = "REI"
+# tabuleiro_vazio[-1][4] = "RAI"
+# tabuleiro_vazio[-2] = ["PEA"] * 8
+
+
+# for linha in tabuleiro_vazio:
+#     print(linha)
--- Aula7/tupla_dicionario_conjunto.py
+++ Aula7/tupla_dicionario_conjunto.py
@@ -1,15 +1,41 @@
 ### TUPLA - CONJUNTO - DICIONÁRIO
 #tupla - necessario a ',' para ser uma tupla, caso não tenha, ela é uma str
 
-tupla_vazia = ()
-print(type(tupla_vazia))
-print(tupla_vazia)
+# tupla_vazia = ()
+# print(type(tupla_vazia))
+# print(tupla_vazia)
 
-tupla = ("casa", 2, [1,2,3], tupla_vazia)
-tupla[2].append(6)
-print(tupla)
+# tupla = ("casa", 2, [1,2,3], tupla_vazia)
+# tupla[2].append(6)
+# print(tupla)
 
-tupla_nomes = ("joão", "Pedro", "Sara")
-print(type(tupla_nomes))
-lista_nomes = list(tupla_nomes) #transformanso a tupla em lista
-lista_nomes
+# tupla_nomes = ("joão", "Pedro", "Sara")
+# print(type(tupla_nomes))
+# lista_nomes = list(tupla_nomes) #transformanso a tupla em lista
+# lista_nomes
+
+#####    MATCH
+
+#criando um menu com match (CRUD)
+
+while True:
+    opcao = input("Escola uma opção: \n "
+                 "1 - Adicionar um livro \n"
+                 "2 - Editar informaçoes \n"
+                 "3 - Pesquisar um livro \n"
+                 "4 - Deletar um livro \n"
+                 "0 - Sair \n")
+ 
+    match opcao:
+        case "1":
+            print("adicionando livro")
+        case "2":
+            print("editando livro")
+        case "3":
+            print("pesquisando livro")
+        case "4":
+            print("deletando livro")
+        case "0":
+            break
+        case _:
+            print("opcao invalida")

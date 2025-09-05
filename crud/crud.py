bf2aa9d5c6b6e90487c65e07d2dad8fbc4c66cdb
TalitaCipriano <talitacfmc@gmail.com>
2025-09-05 20:35:45 +0000

    crud

---
+++ crud/crud.py
@@ -0,0 +1,14 @@
+Agora vocês vão botar a mão na massa de fato no CRUD.
+
+A ideia é usar tudo que aprendemos até agora para construir essa ferramenta. O objetivo dessa segunda parte é:
+
+Criar o menu da aplicação
+Criar função para adicionar itens
+Criar função para pesquisar itens
+Criar função para editar itens
+Criar função para remover itens
+Tentem criar arquivos separados para cada função, facilitando a organização do código e evitando incompatibilidades ao trabalhar em equipe.
+
+Ao fim da atividade, todos os membros dos grupos, devem enviar aqui o link do repositório para registrar que a atividade foi feita.
+
+me mostra o passo a passo

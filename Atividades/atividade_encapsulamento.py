22c609bc706777b685b63b03a4796edb6e73a715
TalitaCipriano <talitacfmc@gmail.com>
2025-09-25 19:08:20 +0000

    atividade heranca

--- Atividades/atividade_encapsulamento.py
+++ Atividades/atividade_encapsulamento.py
@@ -1,58 +1,55 @@
 #11111111111111111111111111111111111111111111111111111111111111111
-Na classe, ContaBancaria, converta saldo para uma atributo privado. 
-Crie um método setter e um getter para acessar e modificar essa atributo, seguindo uma regra lógica (Ex: saldo não pode ser negativo)
 
 class ContaBancaria:
-    def __init__(self, nome, agencia, conta, saldo)
+    def __init__(self, nome, agencia, conta, saldo_inicial):
         self.nome = nome
         self.agencia = agencia
         self.conta = conta
-        self.__saldo = saldo_inicial 
-            if saldo_inicial >= 0 
-            else 0
+        self.__saldo = saldo_inicial if saldo_inicial >= 0 else 0
 
-    def get__saldo(self):
+    def get_saldo(self):
         return self.__saldo
 
-    def set_saldo(self, saldo):
+    def set_saldo(self, valor):
         if valor < 0:
             print("Erro: saldo não pode ser negativo.")
         else:
             self.__saldo = valor
 
-conta.set_saldo(500)
-print(conta.get_saldo())
+conta = ContaBancaria("Talita", "2323", "98765", 1000)
 
-     
+conta.set_saldo(500)
+
+print(conta.get_saldo())
 
 #2222222222222222222222222222222222222222222222222222222222222222222222
 
-class Pessoa:
-    def __init__(self, nome, data_nascimento, cpf, rg):
-        self.nome = nome
-        self.data_nascimento = data_nascimento
-        self.__cpf = cpf
-        self.__rg = rg
+# class Pessoa:
+#     def __init__(self, nome, data_nascimento, cpf, rg):
+#         self.nome = nome
+#         self.data_nascimento = data_nascimento
+#         self.__cpf = cpf
+#         self.__rg = rg
 
-    def get__cpf(self):
-        return self.__cpf
+#     def get__cpf(self):
+#         return self.__cpf
     
-    def get__rg(self):
-        return self.__rg
+#     def get__rg(self):
+#         return self.__rg
 
-    def set_cpf(self, cpf):
-        self.__cpf = cpf
+#     def set_cpf(self, cpf):
+#         self.__cpf = cpf
         
-    def set_rg(self, rg):
-        self.__rg = rg
+#     def set_rg(self, rg):
+#         self.__rg = rg
 
-pessoa = Pessoa("Talita", "13/12/1988", "000.000.000-00", "0000000")
-print(pessoa.nome)
-print(pessoa.data_nascimento)
-print(pessoa.get__cpf())
-print(pessoa.get__rg())
+# pessoa = Pessoa("Talita", "13/12/1988", "000.000.000-00", "0000000")
+# print(pessoa.nome)
+# print(pessoa.data_nascimento)
+# print(pessoa.get__cpf())
+# print(pessoa.get__rg())
 
-pessoa.set_cpf("111.111.111-11")
-pessoa.set_rg("1111111")
-print(pessoa.get__cpf())  
-print(pessoa.get__rg())  
+# pessoa.set_cpf("111.111.111-11")
+# pessoa.set_rg("1111111")
+# print(pessoa.get__cpf())  
+# print(pessoa.get__rg())  
--- Atividades/atividade_heranca.py
+++ Atividades/atividade_heranca.py
@@ -1,16 +1,81 @@
-#Crie uma classe Usuario com atributos nome e email. Depois, crie uma classe Cliente que herde de Usuario. 
-#Crie uma instancia de um cliente e acesse todos os atributos.
-
+#111111111111 2222222222222 333333333333 4444444444444
 class Usuario:
-     def __init__(self, nome, email):
-         self.nome = nome
-         self.email = email
+    def __init__(self, nome, email):
+        self.nome = nome
+        self.email = email
+
+    def exibir_informacoes(self):
+        return f"Nome: {self.nome}, E-mail: {self.email}"
+    
+    def saudacao(self):
+        return f"Olá, usuário"
 
 class Cliente(Usuario):
-     def __init__(self, nome, email, tipo_cliente):  
-         super().__init__(nome, email)  
-         self.tipo_cliente = tipo_cliente
+    def __init__(self, nome, email, tipo_cliente, saldo):  
+        super().__init__(nome, email)  
+        self.tipo_cliente = tipo_cliente
+        self.saldo = saldo
 
-cliente1 = Cliente("Talita", "talita@talita.com", "ouro")
+    def saudacao(self):
+        return f"Olá, cliente"
 
-print(f"Nome: {cliente1.nome}, E-mail: {cliente1.email}, Cliente: {cliente1.tipo_cliente}")
+cliente1 = Cliente("Talita", "talita@talita.com", "ouro", 1500.00)
+
+print(cliente1.saudacao())
+print(cliente1.exibir_informacoes())
+print(f"Tipo de cliente: {cliente1.tipo_cliente}, Saldo: R${cliente1.saldo:.2f}")
+
+#555555555555555555555555555555555555555555555555555555555555
+
+class Usuário:
+    def __init__(self, nome, email, id):
+        self.nome = nome
+        self.email = email
+        self.id = id
+
+    def exibir_informacoes(self):
+        return f"Nome: {self.nome}, E-mail: {self.email}, ID: {self.id}"
+
+class Funcionário(Usuário):
+    def __init__(self, nome, email, id, cargo, salario):  
+        super().__init__(nome, email, id)
+        self.cargo = cargo
+        self.salario = salario
+
+    def exibir_informacoes(self):
+        info_base = super().exibir_informacoes()
+        return f"{info_base}, Cargo: {self.cargo}, Salário: R${self.salario:.2f}"
+
+class Gerente(Funcionário):
+    def __init__(self, nome, email, id, cargo, salario, departamento):  
+        super().__init__(nome, email, id, cargo, salario)
+        self.departamento = departamento
+
+    def exibir_informacoes(self):
+        info_funcionario = super().exibir_informacoes()
+        return f"{info_funcionario}, Departamento: {self.departamento}"
+    
+gerente1 = Gerente("Talita", "talita@talita.com", "9273", "Gerente de TI", 14000, "Tecnologia da Informação")
+
+print(gerente1.exibir_informacoes())
+print(f"Nome: {gerente1.nome}")
+print(f"Departamento: {gerente1.departamento}")
+
+#666666666666666666666666666666666666666666666666666666666666666666
+# Crie uma classe Autenticacao com um método login(). 
+# Crie outra classe Permissao com um método verificar_permissao(). 
+# Em seguida, crie uma classe Administrador que herda de ambas. Como usar os métodos herdados?
+
+class Autenticacao:
+    def login(self, usuario, senha)
+    self.usuario = usuario
+    self.senha = senha
+
+class Permissao:
+    def __init__(self, verificar_permissao):
+    self.verificar_permissao = verificar_permissao
+
+class Administrador:
+    def __init__(self,):
+    super().__init__(login, verifcar_permissao)
+        

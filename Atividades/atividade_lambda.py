ebfc5d50bf8537451c89c2fd1338723b17fc6070
TalitaCipriano <talitacfmc@gmail.com>
2025-10-02 18:26:22 +0000

    lambda

--- Atividades/atividade_lambda.py
+++ Atividades/atividade_lambda.py
@@ -1,13 +1,34 @@
-#1 Dobro dos números (map + lambda): Dada a lista [1, 2, 3, 4, 5], use map com lambda para gerar uma nova lista com o dobro de cada número.
-#2 Filtrar pares (filter + lambda): Dada a lista [10, 15, 20, 25, 30], use filter com lambda para obter apenas os números pares.
-#3 Soma dos números (reduce + lambda): Usando reduce, some todos os números da lista [1, 2, 3, 4, 5].
-#4 Ordenação por comprimento da palavra (sorted + lambda): Dada a lista ["uva", "banana", "maçã", "laranja"], ordene as palavras pelo tamanho usando sorted e lambda.
-#5 Primeira letra maiúscula (map + lambda): Dada a lista ["ana", "pedro", "maria"], use map e lambda para transformar em ["Ana", "Pedro", "Maria"].
-#6 Produto dos números (reduce + lambda): Usando reduce, calcule o produto (multiplicação) de todos os elementos da lista [2, 3, 4, 5].
-#7 Ordenar por último caractere (sorted + lambda): Dada a lista ["banana", "uva", "maçã", "laranja"], ordene as palavras pelo último caractere.
+#111111111111111111111111111111111111111111111111111
+lista = [1,2,3,4,5]
+print(list(map(lambda num:num*2, lista)))
 
+#222222222222222222222222222222222222222222222222222
+lista = [10, 15, 20, 25, 30]
+print(list(filter(lambda num:num % 2 == 0, lista)))
 
+#333333333333333333333333333333333333333333333333333
+lista = [1, 2, 3, 4, 5]
+print(list(map(lambda num:num + 1, lista)))
 
+#444444444444444444444444444444444444444444444444444
+frutas = ["uva", "banana", "maçã", "laranja"]
+ordenadas = sorted(frutas, key=lambda x: len(x))
+print(ordenadas)
+
+#555555555555555555555555555555555555555555555555555
+palavras = ["ana", "pedro", "maria"]
+maiusculas = list(map(lambda x: x.capitalize(), palavras))
+print(maiusculas)
+
+#6666666666666666666666666666666666666666666666666666
+from functools import reduce
+numeros = [2,3,4,5]
+print(reduce(lambda x, y : x * y, numeros))
+
+#77777777777777777777777777777777777777777777777777777
+lista = ["banana", "uva", "maçã", "laranja"]
+ordem = sorted(lista, key=lambda x: x[-1])
+print(ordem)
 
 
 

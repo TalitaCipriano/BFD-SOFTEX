6c87337a03df513d2a0ac3f329026d6606972788
TalitaCipriano <talitacfmc@gmail.com>
2025-10-16 17:30:41 +0000

    atividade

--- Atividades/atividade_poo_associacao.py
+++ Atividades/atividade_poo_associacao.py
@@ -40,3 +40,42 @@ aluno1 = Aluno(nome="Talita")
 aluno1.pegar_onibus(onibus1)
 
 #3333333333333333333333333333333333333333333333333333333333333
+
+class Funcionario:
+    def __init__(self, id, nome, cargo):
+        self.id = id
+        self.nome = nome
+        self.cargo = cargo
+
+    def __str__(self):
+        return f"[{self.id}] {self.nome} - {self.cargo}"
+
+
+class Departamento:
+    def __init__(self, nome):
+        self.nome = nome
+        self.funcionarios = []
+
+    def adicionar_funcionario(self, funcionario):
+        if isinstance(funcionario, Funcionario):
+            self.funcionarios.append(funcionario)
+        else:
+            raise TypeError("Apenas instâncias de Funcionario podem ser adicionadas.")
+
+    def listar_funcionarios(self):
+        print(f"Funcionários do departamento {self.nome}:")
+        for funcionario in self.funcionarios:
+            print(f" - {funcionario}")
+
+
+f1 = Funcionario(1, "Thiago", "Engenheiro")
+f2 = Funcionario(2, "Laís", "Analista")
+
+dep_ti = Departamento("TI")
+
+dep_ti.adicionar_funcionario(f1)
+dep_ti.adicionar_funcionario(f2)
+
+dep_ti.listar_funcionarios()
+
+#44444444444444444444444444444444444444444444444444444444444444

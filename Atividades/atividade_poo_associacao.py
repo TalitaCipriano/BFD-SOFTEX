aef8fe4f50051bf9f6c19cc65bf4cf30705057f9
TalitaCipriano <talitacfmc@gmail.com>
2025-10-20 14:35:33 +0000

    atividade

--- Atividades/atividade_poo_associacao.py
+++ Atividades/atividade_poo_associacao.py
@@ -79,3 +79,91 @@ dep_ti.adicionar_funcionario(f2)
 dep_ti.listar_funcionarios()
 
 #44444444444444444444444444444444444444444444444444444444444444
+
+class Jogador:
+    def __init__(self, nome, posicao):
+        self.nome = nome
+        self.posicao = posicao
+
+    def __str__(self):
+        return f"{self.nome} - {self.posicao}"
+
+class Time:
+    def __init__(self, nome_time):
+        self.nome_time = nome_time
+        self.jogadores = []
+
+    def adicionar_jogador(self, jogador):
+        if isinstance(jogador, Jogador):
+            self.jogadores.append(jogador)
+        else:
+            raise TypeError("Só é possível adicionar instâncias da classe Jogador.")
+
+    def listar_jogadores(self):
+        print(f"Jogadores do time {self.nome_time}:")
+        for jogador in self.jogadores:
+            print(f" - {jogador}")
+
+jogador1 = Jogador("Guilherme", "Goleiro")
+jogador2 = Jogador("Thiago", "Zagueiro")
+jogador3 = Jogador("Pedro", "Lateral")
+
+equipe = Time("Beta FC")
+
+equipe.adicionar_jogador(jogador1)
+equipe.adicionar_jogador(jogador2)
+equipe.adicionar_jogador(jogador3)
+equipe.listar_jogadores()
+
+#55555555555555555555555555555555555555555555555555555555555555
+
+class Motor:
+    def __init__(self, potencia):
+        self.potencia = potencia
+
+    def ligar(self):
+        print(f"Motor de {self.potencia}CV ligado.")
+
+    def __del__(self):
+        print("Motor destruído.")
+
+
+class Carro:
+    def __init__(self, modelo, potencia_motor):
+        self.modelo = modelo
+        self.motor = Motor(potencia_motor)
+
+    def ligar(self):
+        print(f"Carro {self.modelo} está ligando...")
+        self.motor.ligar()
+
+    def __del__(self):
+        print(f"Carro {self.modelo} destruído.")
+
+print(">>> Criando o carro")
+meu_carro = Carro("Hillux", 102)
+meu_carro.ligar()
+
+print("\n>>> Apagando o carro")
+del meu_carro  
+
+#66666666666666666666666666666666666666666666666666666666666
+
+class Comodo:
+    def __init__(self, nome):
+        self.nome = nome
+
+class Casa:
+    def __init__(self):
+        self.sala = Comodo("Sala")
+        self.cozinha = Comodo("Cozinha")
+        self.quarto = Comodo("Quarto")
+        self.banheiro = Comodo("Banheiro")
+
+minha_casa = Casa()
+
+print("Cômodos da casa:")
+print(minha_casa.sala.nome)
+print(minha_casa.cozinha.nome)
+print(minha_casa.quarto.nome)
+print(minha_casa.banheiro.nome)
